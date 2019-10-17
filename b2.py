e32b001a2f5ce9afb65242ebaca12686605d461c
yah04dev <46074913+yah04dev@users.noreply.github.com>
2019-10-17 18:34:57 +0200

    Update b2.py

--- b2.py
+++ b2.py
@@ -9,14 +9,12 @@ def bi(how, n1):
     return  
    
 root = tk.Tk()  
-root.title('Calculator')  
+root.title('Calculator of square area By yah04 ')  
 intr = tk.StringVar()   
 
-tk.Label(root, text="calculator of triangle area By yah04").grid(row=1, column=0)  
+tk.Label(root, text="calculator of Square area By yah04").grid(row=1, column=0)  
 tk.Label(root, text="Observation the calculator use just cm (cm2) for calculate ").grid(row=2, column=0)
-tk.Label(root, text="lenght or width").grid(row=3, column=0)  
-how = tk.Label(root)  
-how.grid(row=7, column=2)  
+tk.Label(root, text="lenght or width").grid(row=3, column=0)   
 entryNum1 = tk.Entry(root, textvariable=intr).grid(row=3, column=1)    
 bi = partial(bi, how, intr)  
 buttonCal = tk.Button(root, text="Calculate", command=bi).grid(row=4, column=0)  

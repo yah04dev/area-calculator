08a105cc84661129ec786fe0c611bd58679a1a4e
yah04dev <46074913+yah04dev@users.noreply.github.com>
2019-10-17 18:36:58 +0200

    Update b4.py

--- b4.py
+++ b4.py
@@ -3,7 +3,7 @@ from functools import partial
 from tkinter import messagebox
    
    
-def bi(label_result, n1, n2):  
+def bi( n1, n2):  
     num1 = (n1.get())  
     num2 = (n2.get())  
     messagebox.showinfo("Title", ("Result =", (int(num1)*int(num2)) ,"cm2" ))  
@@ -19,6 +19,6 @@ tk.Label(root, text="lenght").grid(row=3, column=0)
 tk.Label(root, text="width").grid(row=4, column=0)   
 entryNum1 = tk.Entry(root, textvariable=leng).grid(row=3, column=1)  
 entryNum2 = tk.Entry(root, textvariable=wid).grid(row=4 ,column=1)  
-bi = partial(bi, labelResult, leng, wid)  
+bi = partial(bi,  leng, wid)  
 buttonCal = tk.Button(root, text="Calculate", command=bi).grid(row=5, column=0)  
 root.mainloop()  
